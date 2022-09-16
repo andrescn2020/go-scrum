af6975799ed76cb9e63d204f14f6925e91182c9b
andrescn2020 <andrescn_2010@hotmail.com>
2022-09-16 18:04:10 -0300

    Form and some stilish

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,21 @@ import './App.css'
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Go Scrum</h1>
+    <div className="container">
+      <form>
+        <h1>Iniciar sesión</h1>
+        <div>
+          <label>Email</label>
+          <input type="email" name='email'/>
+        </div>
+        <div>
+          <label>Contraseña</label>
+          <input type="password" name='password' />
+        </div>
+        <div>
+          <button type='submit'>Enviar</button>
+        </div>
+      </form>
     </div>
   )
 }
